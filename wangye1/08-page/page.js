8dd650f89bf70c53908f375c7f7b90f0dc53cb2a
用户名 <>
2025-04-23 11:33:19 +0800

    08

---
+++ wangye1/08-page/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+    <h1>自定义内容</h1>  
+    <h2>
+      第一天
+    </h2>
+    h3
+    <p>
+      这是我的第一个页面
+    </p>
+    </div>
+  );
+}
