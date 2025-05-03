7c1371091b35999cae06e49e000bb7c750b06dec
用户名 <>
2025-05-03 19:51:19 +0800

    09

--- my-next-app/src/app/page.js
+++ my-next-app/src/app/page.js
@@ -3,14 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div >
-    <h1>自定义内容</h1>  
-    <h2>
-      第一天
-    </h2>
-    h3
-    <p>
-      这是我的第一个页面
-    </p>
+    <h1>这是来自函数的内容</h1>  
+   <HomeworkCardTitle cardTitle={''}/>
+    <HomeworkCard/>
+    <HomeworkCard/>
     </div>
   );
 }
