306c1297783015522d21566f3b7a1403b6c3e8be
用户名 <>
2025-05-09 17:11:30 +0800

    10

---
+++ tailwind-app/src/app/page.js
@@ -0,0 +1,10 @@
+export default function () {
+  return (
+    <div>
+      <h1 className="text-rose-700 bg-pink-50 text-center">Home</h1>
+      <p className="text-stone-800">Welcome to my homepage!</p>
+    </div>
+  );
+}
+
+  
